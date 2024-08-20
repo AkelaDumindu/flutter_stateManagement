df9804a5848436fcd6facbb8e9d4b5f2de457041
unknown <akeladumindu2000@gmail.com>
2024-08-20 20:24:51 +0530

    created cert page and functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_state_management/pages/product_page.dart';
 import 'package:flutter_state_management/providers/cart_provider.dart';
+import 'package:flutter_state_management/providers/favourite_provider.dart';
 import 'package:provider/provider.dart';
 
 void main() {
   runApp(
-    ChangeNotifierProvider(
-      create: (context) => CartProvider(),
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => CartProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => FavouriteProvider(),
+        ),
+      ],
       child: const MyApp(),
     ),
   );
--- lib/pages/favourite_page.dart
+++ lib/pages/favourite_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_state_management/data/product_data.dart';
+import 'package:flutter_state_management/model/product_model.dart';
+import 'package:flutter_state_management/providers/favourite_provider.dart';
+import 'package:provider/provider.dart';
 
 class FavouritePage extends StatelessWidget {
   const FavouritePage({super.key});
@@ -8,6 +12,47 @@ class FavouritePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Favourite Page"),
+      ),
+      body: Consumer<FavouriteProvider>(
+        builder: (BuildContext context, FavouriteProvider favouriteProvider,
+            Widget? child) {
+          // want get map data as list
+          final favItem = favouriteProvider.favorite.entries
+              .where((entry) => entry.value)
+              .map((e) => e.key)
+              .toList();
+
+          return ListView.builder(
+            itemCount: favItem.length,
+            itemBuilder: (context, index) {
+              final productId = favItem[index];
+              final Product product = ProductData().products.firstWhere(
+                    (element) => element.id == productId,
+                  );
+
+              return Card(
+                child: ListTile(
+                  title: Text(product.name),
+                  subtitle: Text("\$${product.price}"),
+                  trailing: IconButton(
+                    onPressed: () {
+                      favouriteProvider.toggleFavourite(product.id);
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(
+                            "Remove From favourite",
+                          ),
+                          duration: Duration(seconds: 1),
+                        ),
+                      );
+                    },
+                    icon: Icon(Icons.delete),
+                  ),
+                ),
+              );
+            },
+          );
+        },
       ),
     );
   }
